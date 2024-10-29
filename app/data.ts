c646bbca72829509fa826f628d27863c2e62e7f2
scelo-gumede <120778291+scelo-gumede@users.noreply.github.com>
2024-10-29 09:57:41 +0200

    Add files via upload
    project uploaded

---
+++ app/data.ts
@@ -0,0 +1,50 @@
+
+
+
+
+export const links =[
+    {
+        HOME:["Home1","Home2"]
+    },
+    {
+        PAGES:["About Us","Blog","FAQ's","Privacy Policy","Terms of Use"]
+    },
+    {
+        PROPERTIES:[
+            {
+                APARTMENTS:[
+                    "Contemporary Living Oasis",
+                    "Urban Luxe Apartments",
+                    "Townhouse community",
+                    "CozyTown Residence",
+                    "Contemporary Oasis"
+                ],
+                "LUXURY VILLA":[
+                    "Villa Azure",
+                    "Exquisite Oasis Villa",
+                    "Opulent Haven",
+                    "White Haven Oasis",
+                    "Elyson Estate"
+                ],
+                "NEW ARRIVALS":[
+                    "Exquisite Haven",
+                    "Contemporary Living Quarters",
+                    "Villa Azure",
+                    "White Haven Oasis",
+                    "Exquisite Haven",
+                ],
+                SALES:[
+                    "White Haven Oasis",
+                    "Villa Azure",
+                    "Opulent Haven",
+                    "Elysian Estate",
+                    "Townhouse Coventry"
+                ]
+            }
+        ]
+    },{
+            COLLECTION:"Collections"
+    },{
+        CONTACT:"Contact"
+    }
+]
