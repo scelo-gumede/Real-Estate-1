4c6057f23309f9c599e36403eb20d1b82df9a9f6
scelo <scelogumede95@gmail.com>
2024-11-17 15:19:30 +0200

    added effect on button

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,9 +2,11 @@
 import TextField from '@mui/material/TextField';
 import { Autocomplete, Button } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useState } from 'react';
 
 
 export default function Hero(){
+    const[hoverEffect,setHoverEffect]=useState(0)
 
     const options=["Property","House","Apartment"]
     const places=["Durban","Johannesburg","Capetown","Nelspruit"]
@@ -13,8 +15,12 @@ export default function Hero(){
         <section className="flex bg-grey text-white gap-8 justify-center flex-col items-center h-[100vh]">
             <p className="text-5xl leading-11 text-center font-extrabold">Your Way Home Starts Here</p>
             <div className="flex text-grey gap-5">
-                <p className="bg-green hover:cursor-pointer flex justify-center items-center rounded-md font-bold py-3 px-6">For Rent</p>
-                <p className="bg-green hover:cursor-pointer flex justify-center items-center rounded-md font-bold py-3 px-6">For Sale</p>
+                <p onClick={()=>setHoverEffect(1)}  className={` transition-all duration-300 ${hoverEffect == 1 ? "bg-green text-white": "bg-white"} hover:bg-green hover:text-white hover:cursor-pointer relative flex justify-center items-center rounded-md font-bold py-3 px-6`}>For Rent <span className={`bg-green h-5 w-5 absolute -bottom-2 left-[40%] ${hoverEffect == 1? 'bg-green':'hidden'}  rotate-45 `}>  
+                </span></p>
+                <p onClick={()=>setHoverEffect(2)}  className={` transition-all duration-300  ${hoverEffect== 2? "bg-green text-white" : "bg-white"} hover:bg-green hover:text-white hover:cursor-pointer relative flex justify-center items-center rounded-md font-bold py-3 px-6`}>For Sale
+                <span className={`bg-green h-5 w-5 absolute -bottom-2 left-[40%] ${hoverEffect == 2? 'bg-green':'hidden'}  rotate-45 `}>  
+                </span>
+                </p>
             </div>
 
             <div className="bg-transparent rounded-md">
